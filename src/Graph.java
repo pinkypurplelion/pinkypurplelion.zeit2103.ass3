f4400ba223c900c8fe2f013e7cc61022a4e15c1c
alimxhh <alimuhammad@hotmail.com.au>
2022-05-13 15:30:28 +1000

    add edge shell

--- src/Graph.java
+++ src/Graph.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.LinkedList;
 
 
 
@@ -17,6 +18,7 @@ public class Graph implements GraphInterface {
      * holds all nodes (people) in this graph
      */
     protected HashMap<Integer, Node> nodeList = new HashMap<>();
+     
 
     // public Node addNode(Integer id, String name, LocalDate dob, String suburb)
     // {
@@ -29,6 +31,25 @@ public class Graph implements GraphInterface {
     //     return newPers;
     //     // throw exception - if the person is added to the map or regardless?
     // }
+
+    // hasmap of nodes and list of nodes its atached to 
+    // if it does exist yet, make it
+    // if it does exist, get key of node and add the key of the other node to the list within the map
+    // public void addEdge(Node from, Node to)
+    // {
+    //     // if the hashmap doesn't contain the from node,
+    //     if (!nodeList.containsValue(from))
+    //     {
+    //         // add it to the map
+    //         addNode(from.getId(), from.getName(), from.getDateOB(), from.getSuburb());
+    //     } else 
+    //     if (!nodeList.containsValue(to))
+    //     {
+    //         addNode(to.getId(), to.getName(), to.getDateOB(), to.getSuburb());
+    //         nodeList.get()
+    //     }
+    // }
+
 
     /**
      * Test main that creates a graph,
