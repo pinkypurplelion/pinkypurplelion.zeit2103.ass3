f1e124b012c2dca7f8613dc242b2aefdc24b1dbb
Liam Angus <liam.angus@protonmail.com>
2022-05-14 23:18:57 +1000

    SocialNetwork boilerplate for interface methods

--- src/SocialNetwork.java
+++ src/SocialNetwork.java
@@ -1,3 +1,4 @@
+import java.util.List;
 
 /**
  * represents accounts and their relationship as a graph; 
@@ -25,5 +26,53 @@ public class SocialNetwork implements SocialNetworkInterface {
     public static void main(String[] args) {
         SocialNetwork driver = new SocialNetwork();
         System.out.println(driver.sn);
+    }
+
+    /**
+     * reads the data file and builds the graph accordingly; remember to call
+     * this method in the constructor
+     */
+    @Override
+    public void processFile() {
+
+    }
+
+    /**
+     * returns a list of suggested friends. This will return a List<Node> such
+     * that each Node in the list is a friend of a friend that lives in the same
+     * suburb as currentPerson;
+     *
+     * @param currentPerson @Node
+     * @return a list of no more than 5 friends
+     */
+    @Override
+    public List<Node> suggestFriends(Node currentPerson) {
+        return null;
+    }
+
+    /**
+     * it returns all of friends of a given account sorted based on their
+     * birthday with a string representing time until their birthday
+     *
+     * @param currentPerson
+     * @return a string with friends of a given account sorted based on their
+     * birthday with a string representing time until their birthday
+     */
+    @Override
+    public String remindBDEvents(Node currentPerson) {
+        return null;
+    }
+
+    /**
+     * get a list of all names of mutual friends of two friends x and y
+     *
+     * @param x first person
+     * @param y second person
+     * @return a List<String> with all mutual friends, it should contain only
+     * names
+     */
+    @Override
+    public List<String> getMutualFriends(Node x, Node y) {
+        return null;
     }
 }
