5584f1e29b55714cbe3fd950d9a358eaeff0fe1d
Liam Angus <liam.angus@protonmail.com>
2022-05-24 14:24:37 +1000

    processFile test implemented

--- tests/SocialNetworkTest.java
+++ tests/SocialNetworkTest.java
@@ -1,12 +1,26 @@
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SocialNetworkTest {
+    static SocialNetwork socialNetwork;
+    @BeforeAll
+    static void setUp() {
+        socialNetwork = new SocialNetwork();
+    }
 
     @Test
     void processFile() {
-        fail();
+        System.out.println("Testing correct number of nodes present");
+        assertEquals(1002, socialNetwork.sn.nodeList.size());
+        System.out.println("Tested Passed. \nTesting correct information stored for nodes");
+        assertEquals("Minna Whittaker", socialNetwork.sn.nodeList.get(1).getName());
+        assertNotEquals("Minna Whittaker", socialNetwork.sn.nodeList.get(3).getName());
+        assertEquals("Gungahlin", socialNetwork.sn.nodeList.get(9001).getSuburb());
+        System.out.println("Tested Passed. \nTesting friends correctly added");
+        assertEquals(19 ,socialNetwork.sn.nodeList.get(990).adj.size());
+        System.out.println("Tested Passed. \n");
     }
 
     @Test
